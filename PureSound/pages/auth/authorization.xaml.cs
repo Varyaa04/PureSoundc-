4fb882543f4fd1c23976c815114d6ab395406a8f
Varvara Fedorova <fedorova04_varya@mail.ru>
2025-01-18 12:29:19 +0300

    Добавьте файлы проекта.

---
+++ PureSound/pages/auth/authorization.xaml.cs
@@ -0,0 +1,72 @@
+﻿using PureSound.appCurr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PureSound.pages.auth
+{
+    /// <summary>
+    /// Логика взаимодействия для authorization.xaml
+    /// </summary>
+    public partial class authorization : Page
+    {
+        public authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void btnreg_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.frame.Navigate(new registration());
+        }
+
+        private void btnauth_Click(object sender, RoutedEventArgs e)
+        {
+            AuthUser();     
+        }
+
+        public void AuthUser()
+        {
+            try
+            {
+                var password = AppConn.modeldb.usersTable.FirstOrDefault(x => x.userPassword == passtxt.Password);
+                var login = AppConn.modeldb.usersTable.FirstOrDefault(x => x.userLogin == logtxt.Text);
+                var userObj = AppConn.modeldb.usersTable.FirstOrDefault(x => x.userLogin == logtxt.Text && x.userPassword == passtxt.Password);
+                if (password == null)
+                {
+                    MessageBox.Show("Неверный пароль",
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else if (userObj == null)
+                {
+                    MessageBox.Show("Такого пользователя не существует!", "Ошибка авторизации!",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Здравствуйте, " + userObj.userName + "!",
+                                "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                    App.Current.Properties["idUser"] = userObj.idUser;
+                    AppFrame.frame.Navigate(new player.mainPlayer());
+                       
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("Ошибка " + Ex.Message.ToString() + " Критическая работа приложения!",
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
---
+++ PureSound/pages/auth/registration.xaml.cs
@@ -0,0 +1,147 @@
+﻿using PureSound.appCurr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PureSound.pages.auth
+{
+    /// <summary>
+    /// Логика взаимодействия для registration.xaml
+    /// </summary>
+    public partial class registration : Page
+    {
+        public registration()
+        {
+            InitializeComponent();
+            tbname.MaxLength = 25;
+            tblogin.MaxLength = 30;
+            tbpass.MaxLength = 30;
+        }
+
+        private void btnback_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.frame.Navigate(new authorization());
+        }
+
+        private void btnreg_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                try
+                {
+                    if (AppConn.modeldb.usersTable.Any(x => x.userLogin == tblogin.Text && x.userEmail == tbemail.Text))
+                    {
+                        MessageBox.Show("Пользователь с таким логином и email'ом уже существует!",
+                            "Уведомление", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при регистрации: " + ex.Message,
+                        "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+                if (string.IsNullOrWhiteSpace(tblogin.Text) ||
+                    string.IsNullOrWhiteSpace(tbname.Text) ||
+                    string.IsNullOrWhiteSpace(tbpass.Text))
+                {
+                    MessageBox.Show("Все поля должны быть заполнены!",
+                        "Уведомление", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                if (tbpass.Text != tbrepass.Password)
+                {
+                    MessageBox.Show("Пароли не совпадают!",
+                       "Уведомление", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+
+                string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+
+                if (!Regex.IsMatch(tbemail.Text, emailPattern))
+                {
+                    MessageBox.Show("Введите корректный email!",
+                        "Уведомление", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+
+                usersTable userObj = new usersTable()
+                {
+                    userName = tbname.Text,
+                    userEmail = tbemail.Text,
+                    userLogin = tblogin.Text,
+                    userPassword = tbpass.Text
+                };
+                AppConn.modeldb.usersTable.Add(userObj);
+                AppConn.modeldb.SaveChanges();
+                MessageBox.Show("Вы успешно зарегистрировались!",
+                    "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                AppFrame.frame.Navigate(new authorization());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при регистрации: " + ex.Message,
+                    "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        private void tbrepass_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (tbpass.Text != tbrepass.Password || tbrepass.Password == "")
+            {
+                btnreg.IsEnabled = false;
+                tbrepass.Background = Brushes.LightCoral;
+                tbrepass.BorderBrush = Brushes.Red;
+            }
+            else
+            {
+                btnreg.IsEnabled = true;
+                tbrepass.Background = Brushes.LightGreen;
+                tbrepass.BorderBrush = Brushes.Green;
+
+            }
+        }
+
+        private void tbpass_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (tbpass.Text != tbrepass.Password || tbrepass.Password == "")
+            {
+                btnreg.IsEnabled = false;
+                tbrepass.Background = Brushes.LightCoral;
+                tbrepass.BorderBrush = Brushes.Red;
+            }
+            else
+            {
+                btnreg.IsEnabled = true;
+                tbrepass.Background = Brushes.LightGreen;
+                tbrepass.BorderBrush = Brushes.Green;
+
+            }
+        }
+
+        private void tbname_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Key < Key.A || e.Key > Key.Z) && e.Key != Key.Back)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
